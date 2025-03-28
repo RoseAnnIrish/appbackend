37568ed8bd67e9624662eb3ab2a5601d5b9be87b
RoseAnnIrish <roseannirishdelacruz@gmail.com>
2025-03-29 01:06:58 +1300

    logout done - backend

--- appbackend1/urls.py
+++ appbackend1/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from appbackend1.views import RegisterView, LoginView
+from appbackend1.views import RegisterView, LoginView, LogoutView
+
 from rest_framework import routers
 
 router = routers.DefaultRouter()
 urlpatterns = router.urls
 urlpatterns.append(path('register/', RegisterView.as_view(), name='register'))
 urlpatterns.append(path('login/', LoginView.as_view(), name='login'))
+urlpatterns.append(path('logout/', LogoutView.as_view(), name='logout'))
